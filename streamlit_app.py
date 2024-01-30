6eac92479665d29a5cab82f49c8cc8804775e3c2
Carreta <dannyalvc92@hotmail.com>
2024-01-30 12:57:00 -0600

    Update streamlit_app.py
    Minor updates

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 import streamlit
 import pandas
-
-
+import requests
+import snowflake.connector
+from urllib.error import URLError
 
 streamlit.title('My parents new healthy dinner')
 
@@ -30,8 +31,6 @@ fruit_choice = streamlit.text_input('What fruit would you like information about
 streamlit.write('The user entered ', fruit_choice)
 
 
-import requests
-
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 # write your own comment -what does the next line do? 
@@ -39,7 +38,7 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
 streamlit.dataframe(fruityvice_normalized)
 
-import snowflake.connector
+streamlit.stop()
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
