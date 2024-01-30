f6fa869f9361b94abb9b84193f7f456e9145abdd
Carreta <dannyalvc92@hotmail.com>
2024-01-29 18:07:45 -0600

    Update streamlit_app.py
    Updated menu options

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import streamlit
 streamlit.title('My parents new healthy dinner')
 
 streamlit.header('Breakfast menu')
-streamlit.text('Omega 3 & Blueberry oatmeal')
-streamlit.text('Kale, spinach & rocket smoothie')
-streamlit.text('Hard-boiled free-range egg')
+streamlit.text('🥣 Omega 3 & Blueberry oatmeal')
+streamlit.text('🥗 Kale, spinach & rocket smoothie')
+streamlit.text('🐔 Hard-boiled free-range egg')
+streamlit.text('🥑🍞 Avocado toast')
