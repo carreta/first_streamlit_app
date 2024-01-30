de7ba40f33592aa0547793971381d6c1c933bf7e
Carreta <dannyalvc92@hotmail.com>
2024-01-29 18:08:34 -0600

    Update streamlit_app.py
    Added new option to the menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,5 @@ streamlit.text('🥣 Omega 3 & Blueberry oatmeal')
 streamlit.text('🥗 Kale, spinach & rocket smoothie')
 streamlit.text('🐔 Hard-boiled free-range egg')
 streamlit.text('🥑🍞 Avocado toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
